ebf9b1c96a40b418869cbf1bc0c976201e767c60
AlexAvilaPapis <aavilapa@edu.tecnocampus.cat>
2023-03-19 11:06:56 +0100

    aaa

---
+++ Assets/_Scripts/EntityManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+
+public class EntityManager : MonoBehaviour
+{
+    [SerializeField] private List<Entity> Entities;
+    private int _currentIndex;
+
+    public Entity ActiveEntity => Entities[_currentIndex];
+
+    public Entity OtherEntity => Entities[++_currentIndex % Entities.Count];
+
+    public Entity[] Enemies => Entities.Where(x => x.Team != ActiveEntity.Team).ToArray();
+
+
+
+    public void SetNextEntity()
+    {
+        _currentIndex++;
+        _currentIndex = _currentIndex % Entities.Count;
+    }
+
+    public void SetPreviousEntity()
+    {
+        _currentIndex--;
+        if (_currentIndex < 0)
+            _currentIndex = Entities.Count - 1;
+    }
+}
